0da85f851dccefcc16126537af91cec73122becc
Pawel Sioda <pawel.sioda@gmail.com>
2023-09-14 22:57:10 +0200

    style main container in app

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -2,7 +2,7 @@ import { Input } from '@ems/common-ui';
 
 export function App() {
   return (
-    <div>
+    <div className="mx-auto px-8 py-4 text-lg">
       <Input label="Name" type="name" name="name" id="name" />
     </div>
   );
