193e02c3eaafb1beda2ea8c9531a1d4eb0a521f5
Pawel Sioda <pawel.sioda@gmail.com>
2023-09-24 23:56:39 +0200

    delete Input atom component and add FormField atom component

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,11 +1,5 @@
-import { Input } from '@ems/common-ui';
-
 export function App() {
-  return (
-    <div className="mx-auto px-8 py-4 text-lg">
-      <Input label="Name" type="name" name="name" id="name" />
-    </div>
-  );
+  return <div className="mx-auto px-8 py-4 text-lg">Hello, World!</div>;
 }
 
 export default App;
