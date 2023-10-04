0292c8f1f735b4bd164ac4600e0be856255bed52
Pawel Sioda <pawel.sioda@gmail.com>
2023-10-04 22:00:45 +0200

    Create stories for Label and Input

---
+++ libs/common-ui/src/lib/atoms/Label/Label.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Label } from './Label';
+
+const meta = {
+  title: 'UI/Atoms/Label',
+  component: Label,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Label>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    id: 'label_id',
+    children: 'This is label',
+  },
+};
