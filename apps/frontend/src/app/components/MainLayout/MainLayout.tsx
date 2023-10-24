82b69e84f8e6341db79e0bb6af6b5369cde7877c
Pawel Sioda <pawel.sioda@gmail.com>
2023-10-24 22:46:08 +0200

    divide Navbar nad Footer to new components

--- apps/frontend/src/app/components/MainLayout/MainLayout.tsx
+++ apps/frontend/src/app/components/MainLayout/MainLayout.tsx
@@ -1,29 +1,15 @@
-import { Link, Outlet } from 'react-router-dom';
-import { AppRoutes } from '../../AppRoutes';
+import { Outlet } from 'react-router-dom';
+import { Footer } from './Footer';
+import { Navbar } from './Navbar';
 
 export const MainLayout = () => {
   return (
     <div className="min-h-screen flex flex-col justify-between">
-      <nav className="mb-8 p-4">
-        <ul className="flex list-none gap-x-2">
-          <li>
-            <Link to={AppRoutes.HOME}>Home</Link>
-          </li>
-          <li>
-            <Link to={AppRoutes.ABOUT}>About</Link>
-          </li>
-          <li>
-            <Link to={AppRoutes.CONTACT}>Contact</Link>
-          </li>
-          <li>
-            <Link to={AppRoutes.EMPLOYEES}>Employees</Link>
-          </li>
-        </ul>
-      </nav>
+      <Navbar />
       <main className="flex-grow mb-8">
         <Outlet />
       </main>
-      <footer className="p-4 text-center">&copy; 2023 MEWA</footer>
+      <Footer />
     </div>
   );
 };
