64385f13e5afe692887eefe23ac69b71acb512b4
Pawel Sioda <pawel.sioda@gmail.com>
2023-11-03 21:05:54 +0100

    setup msw

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -3,11 +3,26 @@ import * as ReactDOM from 'react-dom/client';
 
 import App from './app/app';
 
+async function deferRender() {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
+
+  const { worker } = await import('./mocks/browser');
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+
+deferRender().then(() => {
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+});
---
+++ apps/frontend/src/mocks/handlers.ts
@@ -0,0 +1,49 @@
+import { http, HttpResponse } from 'msw';
+
+const API_URL = 'https://randomuser.me/api/?results=5';
+const mockedEmployeesResponse = {
+  results: [
+    {
+      name: {
+        first: 'John',
+        last: 'Doe',
+      },
+      location: {
+        street: 'Sezamkowa',
+        city: 'Nowhere',
+        state: 'Xyz',
+        country: 'USA',
+        postcode: '1234',
+      },
+      email: 'johndoe@example.com',
+      id: {
+        name: 'abc',
+        value: '123',
+      },
+    },
+    {
+      name: {
+        first: 'Jan',
+        last: 'Nowak',
+      },
+      location: {
+        street: 'Poziomkowa',
+        city: 'Warszawa',
+        state: '',
+        country: 'Poland',
+        postcode: '10-100',
+      },
+      email: 'jan_nowak@example.com',
+      id: {
+        name: 'cde',
+        value: '456',
+      },
+    },
+  ],
+};
+
+export const handlers = [
+  http.get(API_URL, () => {
+    return HttpResponse.json(mockedEmployeesResponse);
+  }),
+];
